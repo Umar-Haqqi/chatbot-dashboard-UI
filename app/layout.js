bda7e1aae79c7d92710e48dae30ac48f994e0e03
Umar-Haqqi <umer1562001@gmail.com>
2024-01-03 19:11:02 +0500

    MUI general setup for next complete

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+// step 1: copy and paste it here and make v13 to v14
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +14,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+
+        {/* step 2: wrap children */}
+        <AppRouterCacheProvider>
+          {children}
+        </AppRouterCacheProvider>
+
+      </body>
     </html>
   )
 }
